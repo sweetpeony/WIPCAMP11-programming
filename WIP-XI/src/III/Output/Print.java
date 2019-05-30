9914830024b8cd114b36a76d618bb670629c00a3
sweetpeony <ratjittra.voen@gmail.com>
2019-05-30 11:37:25 +0700

    [ADD] New Project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("เหวิน\n");
+        
+        System.out.print("มัทฉะ\n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("โมล มัทฉะ \n");
+        
+        System.out.print("ลิน มัทฉะ ");
+        
+    }
+}
---
+++ WIP-XI/src/III/Output/_2_Printf.java
@@ -0,0 +1,22 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _2_Printf {
+    public static void main(String[] args) {
+        String firstName = "Wippo";
+        String lastName = "Ontour";
+        String nickName = "Wip";
+        double ownWallet = 200.746352;
+        double friendWallet = 500.947855;
+        
+        System.out.printf("สวัสดี เราชื่อ %s %s (%s)\nเรามีเงิน %.3f บาท และเพิ่อนเรามีเงิน %.2f บาท \n", firstName, lastName, nickName, ownWallet, friendWallet);
+
+    }
+}
